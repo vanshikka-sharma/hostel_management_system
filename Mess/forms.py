92044f687c630498289b7436b39374c4085b99df
NIC8146 <52288863+NIC8146@users.noreply.github.com>
2025-09-15 04:23:48 +0530

    Update form styling and enhance event list layout for improved UI

--- Mess/forms.py
+++ Mess/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from .models import MessMenu
 
 class MessMenuForm(forms.ModelForm):
+    breakfast = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    lunch = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    dinner = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    
     class Meta:
         model = MessMenu
         fields = ["day", "breakfast", "lunch", "dinner"]
