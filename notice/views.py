40f81c34f9f850be3967384f97e1831bee49f812
vanshikka-sharma <@vanshikka28@gmail.com>
2025-09-14 23:28:38 +0530

    Add automatic deletion of notices older than 3 months in the base view

--- notice/views.py
+++ notice/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from .models import Notice
 from django.utils import timezone
+from datetime import timedelta
 
 def base(request):
+    # Delete notices older than 3 months
+    three_months_ago = timezone.now() - timedelta(days=90)
+    from .models import Notice
+    Notice.objects.filter(created_at__lt=three_months_ago).delete()
     notices = Notice.objects.order_by('-created_at')
     return render(request, 'notice_base.html', {'notices': notices})
 
